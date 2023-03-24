4baad7668fb1ec306b9c16642ff1ee73a971ee16
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-03-24 10:04:48 +0530

    Changed from axios to fetch

--- src/LoginBasePage/Components/TopContainer/RightContainer/LoginForm/LoginForm.js
+++ src/LoginBasePage/Components/TopContainer/RightContainer/LoginForm/LoginForm.js
@@ -1,5 +1,4 @@
 import { useState, useRef } from 'react';
-import axios from 'axios';
 
 export default function LoginForm() {
     const [loginDetail, setLoginDetail] = useState('');
@@ -13,12 +12,13 @@ export default function LoginForm() {
         setPassword(loginPassword.current.value);
         console.log(`Login info: ${loginDetail} \nLogin password: ${password}`);
 
+        try {
         var data = {
             firstdata: `${loginDetail}`,
             password: `${password}`,
           };
       
-          const url = new URL('http://localhost:3000/signin');
+          const url = new URL('http://192.168.1.149:3000/signin');
           url.search = new URLSearchParams(data).toString();
           var requestOptions = {
               method: "GET",
@@ -28,7 +28,9 @@ export default function LoginForm() {
             .then((response) => response.json())
             .then((result) => console.log(result))
             .catch((error) => console.log("Error shot ->", error));
-
+        } catch(err) {
+            console.log('error-->', err);
+        }
     }
 
 
