3f597b279afc842e87f462ccc52d425cc8a417da
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-04-12 14:58:37 +0530

    Last working state as of 15:00

--- src/components/NewSignup/RegisterBirthdayMobile/RegisterBirthdayMobile.jsx
+++ src/components/NewSignup/RegisterBirthdayMobile/RegisterBirthdayMobile.jsx
@@ -3,6 +3,24 @@ import TopNavBackWithLink from '../../TopNavWithBackLink/TopNavWithBackLink';
 import styles from './RegisterBirthdayMobile.module.scss';
 
 export default function RegisterBirthdayMobile() {
+    const monthArray = ["January", "February", "March",
+                        "April", "May", "June", "July",
+                        "August", "September", "October",
+                        "November", "December"];
+    const monthItems = monthArray.map((item, index) => <option value={index+1}>{item}</option>)
+
+    const dateArray = [];
+    for(let i = 1; i <= 31; i++) {
+        dateArray.push(i);
+    }
+    const dateItems = dateArray.map(i => <option value={i}>{i}</option>);
+
+    const yearArray = [];
+    for(let i = 2023; i >= 1919; i--) {
+        yearArray.push(i);
+    }
+    const yearItems = yearArray.map((i) => <option value={i}>{i}</option>)
+
     return (
         <div>
             <TopNavBackWithLink middleText={"Register"} rightLogo={<></>} options={false}/>
@@ -19,8 +37,23 @@ export default function RegisterBirthdayMobile() {
                     <div className={`${styles.linkText}`}>Why do I need to provide my date of birth?</div>
                 </div>
 
-                <div>
-                    Date selection
+                <div className={`${styles.dateSelectorSection} flex flex-col`}>
+                    <div className={`${styles.infoContainer} flex`}>
+                        <select className={`${styles.selectTag}`}>
+                            {monthItems}
+                        </select>
+
+                        <select className={`${styles.selectTag}`}>
+                            {dateItems}
+                        </select>
+
+                        <select className={`${styles.selectTag}`}>
+                            {yearItems}
+                        </select>
+                    </div>
+                    <div className={`${styles.info} ${styles.infoSmall}`}>
+                        You need to enter the date you were born on
+                    </div>
                 </div>
 
                 <div className={`${styles.info} ${styles.infoSmall}`}>
