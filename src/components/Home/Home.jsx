50364069397c79c2839824437a2fe05910bdd412
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-05-10 10:50:34 +0530

    Last working state as of 10:50

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,24 @@
-import axios from 'axios';
+import axios from "axios";
 
-import TopNavbar from '../TopNavbar/TopNavbar';
-import StoryBar from '../StoryBar/StoryBar';
-import Navbar from '../LeftNavbar/LeftNavbar';
-import SuggestedForYou from '../SuggestedForYou/SuggestedForYou';
-import CompletelyCaughtUp from '../CompletelyCaughtUp/CompletelyCaughtUp';
-import MobilePost from '../MobilePost/MobilePost';
+import TopNavbar from "../TopNavbar/TopNavbar";
+import StoryBar from "../StoryBar/StoryBar";
+import Navbar from "../LeftNavbar/LeftNavbar";
+import SuggestedForYou from "../SuggestedForYou/SuggestedForYou";
+import CompletelyCaughtUp from "../CompletelyCaughtUp/CompletelyCaughtUp";
+import MobilePost from "../MobilePost/MobilePost";
 
-import styles from './Home.module.scss';
+import styles from "./Home.module.scss";
 
 export default function Home() {
-    // useEffect(() => {
-    //     axios.get()
-    // }, [])
-    return (
-        <>
-            <TopNavbar />
-            <StoryBar />
-            <h1>Home</h1>
-            <SuggestedForYou />
-            <CompletelyCaughtUp />
-            <MobilePost />
-            <Navbar />
-        </>
-    );
+  return (
+    <>
+      <TopNavbar />
+      <StoryBar />
+      <h1>Home</h1>
+      <SuggestedForYou />
+      <CompletelyCaughtUp />
+      <MobilePost />
+      <Navbar />
+    </>
+  );
 }
